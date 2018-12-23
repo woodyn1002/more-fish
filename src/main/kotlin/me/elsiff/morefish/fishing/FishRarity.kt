6b35e2297ceb3621523f423585d424c5cd0a0d50
elsiff <elsiff1002@gmail.com>
2018-12-24 01:14:27 +0900

    Apply Kotlin coding convention to constructors

--- src/main/kotlin/me/elsiff/morefish/fishing/FishRarity.kt
+++ src/main/kotlin/me/elsiff/morefish/fishing/FishRarity.kt
@@ -5,6 +5,17 @@ import org.bukkit.ChatColor
 /**
  * Created by elsiff on 2018-12-23.
  */
-data class FishRarity(val name: String, val displayName: String, val chance: Double, val color: ChatColor, val feature: Feature) {
-    data class Feature(val additionalPrice: Double, val noBroadcast: Boolean, val noDisplay: Boolean, val firework: Boolean)
+data class FishRarity(
+        val name: String,
+        val displayName: String,
+        val chance: Double,
+        val color: ChatColor,
+        val feature: Feature
+) {
+    data class Feature(
+            val additionalPrice: Double,
+            val noBroadcast: Boolean,
+            val noDisplay: Boolean,
+            val firework: Boolean
+    )
 }
--- src/main/kotlin/me/elsiff/morefish/fishing/FishType.kt
+++ src/main/kotlin/me/elsiff/morefish/fishing/FishType.kt
@@ -5,7 +5,16 @@ import org.bukkit.inventory.ItemStack
 /**
  * Created by elsiff on 2018-12-20.
  */
-data class FishType(val name: String, val displayName: String, val lengthMin: Double, val lengthMax: Double,
-                    val icon: ItemStack, val feature: Feature) {
-    data class Feature(val skipItemFormat: Boolean = false, val commands: List<String> = emptyList())
+data class FishType(
+        val name: String,
+        val displayName: String,
+        val lengthMin: Double,
+        val lengthMax: Double,
+        val icon: ItemStack,
+        val feature: Feature
+) {
+    data class Feature(
+            val skipItemFormat: Boolean = false,
+            val commands: List<String> = emptyList()
+    )
 }
