5fca8842ed438e68288ab75c970251eb01c211f6
elsiff <elsiff1002@gmail.com>
2019-01-20 20:13:15 +0900

    Change Record class to data class

--- src/main/kotlin/me/elsiff/morefish/fishing/competition/Record.kt
+++ src/main/kotlin/me/elsiff/morefish/fishing/competition/Record.kt
@@ -6,7 +6,7 @@ import org.bukkit.OfflinePlayer
 /**
  * Created by elsiff on 2018-12-25.
  */
-class Record(val fisher: OfflinePlayer, val fish: Fish) : Comparable<Record> {
+data class Record(val fisher: OfflinePlayer, val fish: Fish) : Comparable<Record> {
     override fun compareTo(other: Record): Int {
         return fish.length.compareTo(other.fish.length)
     }
