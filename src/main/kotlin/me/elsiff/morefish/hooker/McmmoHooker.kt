4f8f2fe3083962a0cf52dacea999beae9d58ae37
LazyGon <lazygon.mc@gmail.com>
2020-10-05 20:52:43 +0900

    [Fix] Prevent mcmmo from modifing fish itemmeta

--- src/main/kotlin/me/elsiff/morefish/hooker/McmmoHooker.kt
+++ src/main/kotlin/me/elsiff/morefish/hooker/McmmoHooker.kt
@@ -1,8 +1,13 @@
 package me.elsiff.morefish.hooker
 
 import com.gmail.nossr50.api.ExperienceAPI
+import com.gmail.nossr50.events.skills.fishing.McMMOPlayerFishingTreasureEvent
+import com.gmail.nossr50.events.skills.fishing.McMMOPlayerMagicHunterEvent
 import me.elsiff.morefish.MoreFish
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
 
 /**
  * Created by elsiff on 2019-01-20.
@@ -12,9 +17,27 @@ class McmmoHooker : PluginHooker {
     override var hasHooked: Boolean = false
 
     override fun hook(plugin: MoreFish) {
+        Bukkit.getPluginManager().registerEvents(McmmoFishingFixer(plugin), plugin)
         hasHooked = true
     }
 
     fun skillLevelOf(player: Player, skillType: String): Int =
-        ExperienceAPI.getLevel(player, skillType)
+            ExperienceAPI.getLevel(player, skillType)
+
+    class McmmoFishingFixer(val plugin: MoreFish): Listener {
+
+        @EventHandler(ignoreCancelled = true)
+        fun onMcmmoFishOnFishContest(event: McMMOPlayerFishingTreasureEvent) {
+            if (plugin.competition.isEnabled()) {
+                event.isCancelled = true
+            }
+        }
+
+        @EventHandler(ignoreCancelled = true)
+        fun onMcmmoFishOnFishContest(event: McMMOPlayerMagicHunterEvent) {
+            if (plugin.competition.isEnabled()) {
+                event.isCancelled = true
+            }
+        }
+    }
 }
